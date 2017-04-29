aa935e11267b5e9b0a68692d4c9fe3052e6dd8e3
Rob Sliwa <robjsliwa@gmail.com>
2017-04-29 16:51:33 -0400

    Added application logging

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"log"
 	"net/http"
+	"os"
 
+	"github.com/go-kit/kit/log"
 	httptransport "github.com/go-kit/kit/transport/http"
 )
 
@@ -27,7 +28,11 @@ type countResponse struct {
 }
 
 func main() {
-	svc := stringService{}
+	logger := log.NewLogfmtLogger(os.Stderr)
+
+	var svc StringService
+	svc = stringService{}
+	svc = loggingMiddleware{logger, svc}
 
 	uppercaseHandler := httptransport.NewServer(
 		makeUppercaseEndpoint(svc),
@@ -43,5 +48,5 @@ func main() {
 
 	http.Handle("/uppercase", uppercaseHandler)
 	http.Handle("/count", countHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	logger.Log(http.ListenAndServe(":8080", nil))
 }
